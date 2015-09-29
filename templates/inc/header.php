cae3c3e35e923005e33eeec4cd1ed402573e949d
Travis Smith <t@wpsmith.net>
2015-09-29 16:29:44 -0400

    Add wp_head()

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -15,7 +15,7 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<?php wp_head(); ?>
 
 	<?php Kanban_Template::add_style() ?>
 </head>
